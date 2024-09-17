8ee43ec145a1f71b10e9d15bec724e64e106a2d4
AlenPAji <99082266+AlenPAji@users.noreply.github.com>
2024-09-17 22:00:34 +0530

    some ui updation

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,34 @@
+import MeetingTypelist from '@/components/MeetingTypelist';
 import React from 'react'
 
 const Home = () => {
+  const currentDate = new Date();
+  const customDate = currentDate.toLocaleDateString('en-US', {
+    weekday: 'long', // e.g., "Tuesday"
+    year: 'numeric',
+    month: 'long', // e.g., "September"
+    day: 'numeric',
+  });
+  const formattedTime = currentDate.toLocaleTimeString('en-US', {
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true, // This ensures that the time is displayed in 12-hour format with AM/PM
+  });
   return (
     <section className='flex size-full flex-col gap 10 text-white'>
-      <h1 className='text-3xl font-bold'>Home</h1>
+      <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
+       <div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11'>
+        <h2 className='glassmorphism max-w-[270px]
+        rounded py-2 text-center text-base font-normal'>Upcoming Meeting at:12:30pm</h2>
+        <div className='flex flex-col gap-2'>
+          <h1 className='text-4xl font-extrabold lg:text-7xl'>
+            {formattedTime}</h1>
+            <p className='text-lg font-medium text-sky-1'>{customDate}</p></div>  
+        </div>
+  
+        </div>
+        <MeetingTypelist/>
+         
     </section>
   )
 }
